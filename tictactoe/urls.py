3676b675d3ed6168c14f5c148ee6543454aa385c
Michael F. Lamb <mike@datagrok.org>
2011-02-07 12:45:50 -0500

    Fix relocatable static path for development
    (What's the django standard practice here?)

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 urlpatterns = patterns('')
 
 if settings.DEBUG:
+    from os.path import abspath, dirname, join
     urlpatterns = patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': 'tictactoe/static'}),
+         {'document_root': join(dirname(abspath(__file__)), 'static')}),
     )
 
 urlpatterns += patterns('tictactoe.views',
